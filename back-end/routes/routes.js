8724c7044c3428d1cf4efc84136405d55a5f67e7
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-07-29 15:34:39 +0800

    Reapply "Add Repair Controller Average working Days Per"
    This reverts commit 7b82496edbf1489b36f237f3af3a8025db388be7.

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -33,6 +33,8 @@ app.post('/TDPMpost', repairController.getTopDefectsPerItemModel);
 app.post('/PTPMpost', repairController.getPendingStatusPerItemModel);
 //Total Item Quantity Per Technician
 app.post('/TIQPTpost', repairController.getTotalItemQuantityPerTechnician);
+//Average working days per technician
+app.post('/AWDPTpost', repairController.getAverageWorkingDaysPerTechnician);
 
 app.get('/table', repairController.getAllRepairs);
 
