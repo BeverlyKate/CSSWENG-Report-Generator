c0ad179866989948aedd24ce724852e73978a4dd
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2024-07-29 23:50:47 +0800

    Added cors config & new routes

--- back-end/app.js
+++ back-end/app.js
@@ -5,6 +5,9 @@ const routes = require("./routes/routes.js");
 const connect = require("./public/database/server.js");
 const appExp = express();
 const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+appExp.use(cors(corsOptions));
 
 dotenv.config();
 
@@ -13,21 +16,21 @@ appExp.set("view engine", "hbs");
 appExp.set("views", path.join(__dirname, "views"));
 // hbs.registerPartials(__dirname + "/views/partials")
 
-appExp.use(bodyParser.json())
-appExp.use(bodyParser.urlencoded( {extended: true } ))
+appExp.use(bodyParser.json());
+appExp.use(bodyParser.urlencoded({ extended: true }));
 //Set static directory
 appExp.use(express.static(__dirname + "/public"));
 //User a router for controllers
 appExp.use("/", routes.app);
 
 //Run on Local Host and connect to Mongo DB
-appExp.listen(process.env.SERVER_PORT, async function(){
+appExp.listen(process.env.SERVER_PORT, async function () {
   console.log("Running on port: " + process.env.SERVER_PORT);
-  try{
-      await connect();
-      console.log("connected to database!");
-  }catch(err){
-      console.error(err);
-      console.log("Failed to connect to database");
+  try {
+    await connect();
+    console.log("connected to database!");
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to connect to database");
   }
 });
---
+++ back-end/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://www.kevlerReportGenerator.com",
+  "http://kevlerReportGenerator.com",
+];
+
+module.exports = allowedOrigins;
