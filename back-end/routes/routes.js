6267d1c281573285c12c53a7780766010c018025
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-08-02 07:46:10 +0800

    Revert "Added cors config & new routes"
    This reverts commit c0ad179866989948aedd24ce724852e73978a4dd.

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -7,18 +7,17 @@ const app = express.Router();
 async function dynamicImport() {
   try {
     // Dynamically import the module
-    const { default: importController } = await import(
-      "../controllers/importController.mjs"
-    );
+      const { default: importController } = await import("../controllers/importController.mjs");
 
     // Now you can use the methods from importController
-    //   importController.getFile
-    app.post("/importFile", importController.importFile);
+  //   importController.getFile
+      app.post("/importFile", importController.importFile);
     // ... other logic ...
+
   } catch (error) {
     console.error("Error importing importController:", error);
-  }
-}
+  };
+};
 dynamicImport();
 const mainController = require("../controllers/mainController.js");
 const repairController = require("../controllers/repairController.js");
@@ -37,18 +36,13 @@ app.post("/PTPMpost", repairController.getPendingStatusPerItemModel);
 //Total Item Quantity Per Technician
 app.post("/TIQPTpost", repairController.getTotalItemQuantityPerTechnician);
 //Total item quantity per model per technician
-app.post(
-  "/TIQPMPTpost",
-  repairController.getTotalItemQuantityPerItemModelPerTechnician
-);
+app.post("/TIQPMPTpost", repairController.getTotalItemQuantityPerItemModelPerTechnician);
 //Average working days per technician
 app.post("/AWDPTpost", repairController.getAverageWorkingDaysPerTechnician);
 //Update from db
-app.post("/update", dbController.update);
-// app.post('/update', dbController.update);
+app.post('/update', dbController.update);
 //Delete from db
-app.post("/delete", dbController.delete);
-// app.post("/delete", dbController.delete);
+app.post('/delete', dbController.delete);
 
 app.get("/table", repairController.getAllRepairs);
 
@@ -56,12 +50,12 @@ app.get("/import", mainController.getImport);
 
 app.get("/home", mainController.getHome);
 
-app.get("/passwordRecovery", mainController.getRecovery);
+app.get('/passwordRecovery', mainController.getRecovery);
 
-app.post("/recovery", mainController.postRecovery);
+app.post('/recovery', mainController.postRecovery);
 
-app.get("/registerUser", mainController.getRegister);
+app.get('/registerUser',mainController.getRegister);
 
-app.post("/newUser", mainController.postRegister);
+app.post('/newUser',mainController.postRegister);
 
 module.exports = { app, dynamicImport };
