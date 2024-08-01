2550c213ac7ce507fc5a3ce63bf3e8cdcc2b7902
VL Dionela <valiantlance.dionela@gmail.com>
2024-08-01 21:11:15 +0800

    Edited cypress routes

--- cypress/e2e/PTPM.cy.js
+++ cypress/e2e/PTPM.cy.js
@@ -1,10 +1,10 @@
 describe('Pending Tasks Per Model (PTPM) reports.', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3001/dash')
-    //cy.get('#username').type('Admin')
-    //cy.get('#password').type('12345678')
-    //cy.get('#login').click()
+    cy.visit('http://localhost:3000/')
+    cy.get('#username').type('Neko Nyan')
+    cy.get('#password').type('catto')
+    cy.get('#login').click()
     cy.get('[data-report="PTPM"]').click()
   })
 
