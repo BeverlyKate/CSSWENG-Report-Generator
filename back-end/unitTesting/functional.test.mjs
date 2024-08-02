7fa3e8d1ac51eba7cbd6633fe5f32539b4c07d37
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-08-02 08:53:40 +0800

    Final edits

--- back-end/unitTesting/functional.test.mjs
+++ back-end/unitTesting/functional.test.mjs
@@ -3,6 +3,8 @@ import importController, { insertRepair } from "../controllers/functionalTestCon
 import mainController from "../controllers/mainController.js";
 import repairModel from "../models/repairSchema.js";
 import repairIdModel from "../models/repairIdSchema.js";
+import User from "../models/userSchema.js";
+import bcrypt from "bcrypt";
 import { IncomingForm } from "formidable";
 
 describe("Import Controller", () => {
@@ -112,40 +114,48 @@ describe("Main Controller", () => {
     }),
 
     describe("Home", () => {
-        // it("should render home if username and password is correct", async function() {
-        //     // Create a mock response object
-        //     const mockRes = { render: jest.fn() };
+        it("should render home if username and password is correct", async function() {
+            // Create a mock response object
+            const mockRes = { render: jest.fn() };
+            const mockFindOne = jest.fn();
+            const mockCompare = jest.fn();
 
-        //     // Create a mock request object
-        //     const mockReq = { 
-        //         body: { username: "Admin", password: "12345678" },
-        //         headers: {
-        //         host: 'localhost:3000',
-        //         connection: 'keep-alive',
-        //         'content-length': '16852305',
-        //         'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        //         accept: '*/*',
-        //         'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryyjm1HU0NM68qybG8',
-        //         'x-requested-with': 'XMLHttpRequest',
-        //         'sec-ch-ua-mobile': '?0',
-        //         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-        //         'sec-ch-ua-platform': '"Windows"',
-        //         origin: 'http://localhost:3000',
-        //         'sec-fetch-site': 'same-origin',
-        //         'sec-fetch-mode': 'cors',
-        //         'sec-fetch-dest': 'empty',
-        //         referer: 'http://localhost:3000/import',
-        //         'accept-encoding': 'gzip, deflate, br, zstd',
-        //         'accept-language': 'en-US,en;q=0.9,tl;q=0.8'
-        //     }};
+            // Mock the repairIdModel save method
+            jest.spyOn(User, "findOne").mockImplementation(mockFindOne);
+            // Mock the repairModel inserMany method
+            jest.spyOn(bcrypt, "compare").mockImplementation((password, hash) => {
+                return Promise.resolve(true);
+            });
 
-        //     // Call the getMain function
-        //     await mainController.getMain(mockReq, mockRes).then(() => {
-        //         expect(mockRes.render).toHaveBeenCalledWith('home');
-        //     }).catch(error => {
-        //         console.log("getMain error: " + error);
-        //     });
-        // }),
+            // Create a mock request object
+            const mockReq = { 
+                body: { username: "Hatdog", password: "hatdog" },
+                headers: {
+                host: 'localhost:3000',
+                connection: 'keep-alive',
+                'content-length': '16852305',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+                accept: '*/*',
+                'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryyjm1HU0NM68qybG8',
+                'x-requested-with': 'XMLHttpRequest',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+                'sec-ch-ua-platform': '"Windows"',
+                origin: 'http://localhost:3000',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                referer: 'http://localhost:3000/import',
+                'accept-encoding': 'gzip, deflate, br, zstd',
+                'accept-language': 'en-US,en;q=0.9,tl;q=0.8'
+            }};
+            const hashedPassword = "$2b$10$AQl0pFrWNYzriQRDeNvYBOCIr8rYmU8Ut/FHmhhQPoQbZtTXfsX4y"
+
+            const result = await bcrypt.compare(mockReq.password, hashedPassword);
+    
+            expect(bcrypt.compare).toHaveBeenCalledWith(mockReq.password, hashedPassword);
+            expect(result).toBe(true);
+        })
 
         // it("should render login if username is incorrect", async function() {
         //     // Create a mock response object
